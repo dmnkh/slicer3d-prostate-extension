f4f8ea231a16ceaa50ac8416f331f99ed29d742f
Dominik Hofer <dominik.hofer@meduniwien.ac.at>
2015-09-16 14:47:39 +0200

    fixed slice alignment

--- Prostate/Prostate.py
+++ Prostate/Prostate.py
@@ -440,8 +440,10 @@ class ProstateTest(ScriptedLoadableModuleTest):
 class DataManager:
   
   layout = ("<layout type=\"vertical\" split=\"true\" >"
+                  " <item>"
+      "  <layout type=\"horizontal\">"
       " <item>"
-      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Red\">"
+      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Main\">"
       "   <property name=\"orientation\" action=\"default\">Axial</property>"
       "   <property name=\"viewlabel\" action=\"default\">R</property>"
       "   <property name=\"viewcolor\" action=\"default\">#F34A33</property>"
@@ -450,14 +452,38 @@ class DataManager:
       " <item>"
       "  <layout type=\"horizontal\">"
       " <item>"
-      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Yellow\">"
+      "  <layout type=\"vertical\">"
+      " <item>"
+      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Coronal\">"
+      "   <property name=\"orientation\" action=\"default\">Sagittal</property>"
+      "   <property name=\"viewlabel\" action=\"default\">Y</property>"
+      "   <property name=\"viewcolor\" action=\"default\">#EDD54C</property>"
+      "  </view>"
+      " </item>"
+      " <item>"
+      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Sagittal\">"
+      "   <property name=\"orientation\" action=\"default\">Axial</property>"
+      "   <property name=\"viewlabel\" action=\"default\">G</property>"
+      "   <property name=\"viewcolor\" action=\"default\">#6EB04B</property>"
+      "  </view>"
+      " </item>"
+      " </layout>"     
+      " </item>"
+      " </layout>"     
+      " </item>"
+      "</layout>"
+      " </item>"
+      " <item>"
+      "  <layout type=\"horizontal\">"
+      " <item>"
+      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Axial\">"
       "   <property name=\"orientation\" action=\"default\">Axial</property>"
       "   <property name=\"viewlabel\" action=\"default\">Y</property>"
       "   <property name=\"viewcolor\" action=\"default\">#EDD54C</property>"
       "  </view>"
       " </item>"
       " <item>"
-      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Green\">"
+      "  <view class=\"vtkMRMLSliceNode\" singletontag=\"Histology\">"
       "   <property name=\"orientation\" action=\"default\">Axial</property>"
       "   <property name=\"viewlabel\" action=\"default\">G</property>"
       "   <property name=\"viewcolor\" action=\"default\">#6EB04B</property>"
@@ -496,20 +522,21 @@ class DataManager:
     for node in sliceNodes:
       node.RotateToVolumePlane(volumeNodes[0])
   
-    for color in ['Red', 'Yellow', 'Green']:
-      slicer.app.layoutManager().sliceWidget(color).sliceLogic().GetSliceCompositeNode().SetForegroundVolumeID(self.histo.GetID())
-      slicer.app.layoutManager().sliceWidget(color).sliceLogic().GetSliceCompositeNode().SetForegroundOpacity(0.5)
-      slicer.app.layoutManager().sliceWidget(color).sliceLogic().GetSliceCompositeNode().SetBackgroundVolumeID(self.mri.GetID())      
-      #slicer.app.layoutManager().sliceWidget(color).fitSliceToBackground()
-      slicer.app.layoutManager().sliceWidget(color).sliceLogic().FitSliceToAll()
+#     for color in ['Red', 'Yellow', 'Green']:
+#       slicer.app.layoutManager().sliceWidget(color).sliceLogic().GetSliceCompositeNode().SetForegroundVolumeID(self.histo.GetID())
+#       slicer.app.layoutManager().sliceWidget(color).sliceLogic().GetSliceCompositeNode().SetForegroundOpacity(0.5)
+#       slicer.app.layoutManager().sliceWidget(color).sliceLogic().GetSliceCompositeNode().SetBackgroundVolumeID(self.mri.GetID())      
+#       #slicer.app.layoutManager().sliceWidget(color).fitSliceToBackground()
+#       slicer.app.layoutManager().sliceWidget(color).sliceLogic().FitSliceToAll()
       
     # Main view with both MRI and histology
-    node = slicer.app.layoutManager().sliceWidget('Red').sliceLogic().GetSliceCompositeNode()
+    node = slicer.app.layoutManager().sliceWidget('Main').sliceLogic().GetSliceCompositeNode()
+    
     node.SetForegroundVolumeID(self.histo.GetID())
     node.SetForegroundOpacity(0.5)
     node.SetBackgroundVolumeID(self.mri.GetID())      
     #slicer.app.layoutManager().sliceWidget(color).fitSliceToBackground()
-    slicer.app.layoutManager().sliceWidget('Red').sliceLogic().FitSliceToAll()
+    slicer.app.layoutManager().sliceWidget('Main').sliceLogic().FitSliceToAll()
 #     newFOVx = node.GetFieldOfView()[0] * 0.7
 #     newFOVy = node.GetFieldOfView()[1] * 0.7
 #     newFOVz = node.GetFieldOfView()[2]
@@ -518,11 +545,11 @@ class DataManager:
 #     node.UpdateMatrices()      
 
     # MRI only view
-    node = slicer.app.layoutManager().sliceWidget('Yellow').sliceLogic().GetSliceCompositeNode()
+    node = slicer.app.layoutManager().sliceWidget('Axial').sliceLogic().GetSliceCompositeNode()
     node.SetForegroundVolumeID(None)
     node.SetBackgroundVolumeID(self.mri.GetID())
-    slicer.app.layoutManager().sliceWidget('Yellow').sliceLogic().FitSliceToAll()
-    node = slicer.util.getNode('*SliceNodeYellow*')
+    slicer.app.layoutManager().sliceWidget('Axial').sliceLogic().FitSliceToAll()
+    node = slicer.util.getNode('*SliceNodeAxial*')
     node.SetOrientationToAxial()
     node.RotateToVolumePlane(volumeNodes[0])
 #     newFOVx = node.GetFieldOfView()[0] * 2
@@ -532,12 +559,43 @@ class DataManager:
 #     node.UpdateMatrices()  
     
     # Histology only view
-    node = slicer.app.layoutManager().sliceWidget('Green').sliceLogic().GetSliceCompositeNode()
+    node = slicer.app.layoutManager().sliceWidget('Histology').sliceLogic().GetSliceCompositeNode()
     node.SetBackgroundVolumeID(self.histo.GetID())
-    node = slicer.util.getNode('*SliceNodeGreen*')
+    node = slicer.util.getNode('*SliceNodeHistology*')
     node.SetOrientationToAxial()
-    slicer.app.layoutManager().sliceWidget('Green').sliceLogic().FitSliceToAll()
+    #slicer.app.layoutManager().sliceWidget('Histology').sliceLogic().FitSliceToAll()
     node.RotateToVolumePlane(volumeNodes[0])
+    
+    # Coronal view
+    node = slicer.app.layoutManager().sliceWidget('Coronal').sliceLogic().GetSliceCompositeNode()
+    node.SetForegroundVolumeID(self.histo.GetID())
+    node.SetBackgroundVolumeID(self.mri.GetID())
+    node.SetForegroundOpacity(0.5)
+    node = slicer.util.getNode('*SliceNodeCoronal*')
+    node.SetOrientationToCoronal()
+    slicer.app.layoutManager().sliceWidget('Coronal').sliceLogic().FitSliceToAll()
+    node.RotateToVolumePlane(volumeNodes[0])    
+    
+    # Sagittal view
+    node = slicer.app.layoutManager().sliceWidget('Sagittal').sliceLogic().GetSliceCompositeNode()
+    node.SetForegroundVolumeID(self.histo.GetID())
+    node.SetBackgroundVolumeID(self.mri.GetID())
+    node.SetForegroundOpacity(0.5)
+    node = slicer.util.getNode('*SliceNodeSagittal*')
+    node.SetOrientationToSagittal()
+    slicer.app.layoutManager().sliceWidget('Sagittal').sliceLogic().FitSliceToAll()
+    node.RotateToVolumePlane(volumeNodes[0])
+    
+    slicer.app.layoutManager().sliceWidget('Histology').fitSliceToBackground()
+    mainNode = slicer.app.layoutManager().sliceWidget('Main').sliceLogic().GetSliceCompositeNode()
+    histoOffset = slicer.app.layoutManager().sliceWidget('Histology').sliceLogic().GetSliceOffset()
+    print(histoOffset)
+    print(type(histoOffset))
+    #slicer.app.layoutManager().sliceWidget('Main').sliceLogic().SetSliceOffset(histoOffset)
+    node = slicer.util.getNode('*SliceNodeMain*')
+    node.SetSliceOffset(float(histoOffset))
+    node = slicer.util.getNode('*SliceNodeHistology*')
+    node.SetSliceOffset(float(histoOffset))
 
   def loadMRI(self):
     volumeLoaded = slicer.util.openAddVolumeDialog()
